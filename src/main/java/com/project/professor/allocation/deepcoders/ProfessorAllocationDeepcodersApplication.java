3223a89a0f0d36cc4805677d793c79a56c58f77e
Marcelo Maranhão <marcelo.maranhao@gmail.com>
2022-08-20 15:18:56 -0300

    Reuploaded project

---
+++ src/main/java/com/project/professor/allocation/deepcoders/ProfessorAllocationDeepcodersApplication.java
@@ -0,0 +1,13 @@
+package com.project.professor.allocation.deepcoders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessorAllocationDeepcodersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessorAllocationDeepcodersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/professor/allocation/deepcoders/ProfessorAllocationDeepcodersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.professor.allocation.deepcoders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfessorAllocationDeepcodersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
