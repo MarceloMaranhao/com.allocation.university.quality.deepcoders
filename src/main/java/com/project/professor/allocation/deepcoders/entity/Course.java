98723fefd2f07e0657652f9133268bc6df724321
Marcelo Maranhão <marcelo.maranhao@gmail.com>
2022-08-20 15:23:33 -0300

    Add Initial Java Models

---
+++ src/main/java/com/project/professor/allocation/deepcoders/entity/Course.java
@@ -0,0 +1,43 @@
+package com.project.professor.allocation.deepcoders.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Course {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long Id;
+	
+	@Column(nullable=false)
+	private String name;
+
+	public Course() {
+		super();
+	}
+
+	public Long getId() {
+		return Id;
+	}
+
+	public void setId(Long id) {
+		Id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Course [Id=" + Id + ", name=" + name + "]";
+	}
+
+}
